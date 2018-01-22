60c3f665b1f966b208686841402c7a16ea04aeab
Steef Min <steefmin@gmail.com>
2018-01-22 14:09:05 +0100

    JSstandard

--- functions/taskapi.js
+++ functions/taskapi.js
@@ -1,14 +1,12 @@
-var Client = require('node-rest-client').Client;
+var Client = require('node-rest-client').Client
 
 module.exports = function (site, input, callback) {
-
-  var client = new Client();
+  var client = new Client()
 
   var args = {
     data: input,
-    headers: { "Content-Type": "application/json" }
-  };
+    headers: { 'Content-Type': 'application/json' }
+  }
 
   client.post(site, args, callback)
 }
-
