bcedd3d87a2b818ca06468125c9d30d058043e63
Steef Min <steefmin@gmail.com>
2018-01-26 10:14:47 +0100

    additionals for gcloud

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ const DialogflowApp = require('actions-on-google').DialogflowApp // Google Assis
 
 let api = require('./sloClient/client')
 
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+exports.dialogflowFulfillment = functions.https.onRequest((request, response) => {
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers))
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body))
   if (request.body.result) {
