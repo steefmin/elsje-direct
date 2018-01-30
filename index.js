fb4532a8a154e9fe2cbdf89fa5750f572f6cd672
Steef Min <steefmin@gmail.com>
2018-01-30 08:21:09 +0100

    renamed base function to compy with strange deploy scheme

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const DialogflowApp = require('actions-on-google').DialogflowApp // Google Assis
 
 let api = require('./sloClient/client')
 
-exports.dialogflowFulfillment = function(request, response) {
+exports.elsjeDirect = function(request, response) {
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers))
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body))
   if (request.body.result) {
